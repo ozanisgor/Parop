e19be189f83444476cfb0de209e2d2caeda4d8fd
ozan isgor <ozanisgor11@gmail.com>
2024-10-07 13:16:09 +0300

    update: next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
-  // experimental: {
-  //   serverComponentsExternalPackages: [
-  //     "puppeteer",
-  //     "puppeteer-core",
-  //     "puppeteer-extra",
-  //     "puppeteer-extra-plugin-stealth",
-  //   ],
-  //   serverMinification: false,
-  // },
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer",
+      "puppeteer-core",
+      "puppeteer-extra",
+      "puppeteer-extra-plugin-stealth",
+      "@sparticuz/chromium-min",
+    ],
+    serverMinification: false,
+  },
 };
 
 export default nextConfig;
