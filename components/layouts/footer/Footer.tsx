454d0b60ec56e11622ec1c57d9fa2e068acc0d4a
ozan isgor <ozanisgor11@gmail.com>
2024-12-17 16:22:00 +0300

    update: footer & navbar width and spacing

--- components/layouts/footer/Footer.tsx
+++ components/layouts/footer/Footer.tsx
@@ -8,25 +8,35 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="flex flex-col justify-between w-full">
-      <div className="bg-primary text-primary-foreground px-16 py-28 grid grid-cols-1 md:grid-cols-3 gap-36">
-        <Contact />
-        <Explore />
-        <Connections />
+      <div className="bg-primary text-primary-foreground">
+        <div className="px-16 py-28 flex justify-between gap-36 max-w-screen-2xl mx-auto">
+          <div className="flex justify-center">
+            <Contact />
+          </div>
+          <div className="flex justify-center">
+            <Explore />
+          </div>
+          <div className="flex justify-center">
+            <Connections />
+          </div>
+        </div>
       </div>
-      <div className="flex justify-between w-full bg-[#2d2f3e] text-primary-foreground h-20 px-20 py-8 items-center">
-        <p className="flex justify-start gap-2 text-sm font-bold">
-          2024 | Ozan İşgör{" "}
-          <Link href={"https://github.com/ozanisgor"}>
-            <FontAwesomeIcon
-              icon={faGithub}
-              size="lg"
-              className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
-            />
+      <div className="w-full bg-[#2d2f3e] text-primary-foreground">
+        <div className="max-w-screen-2xl mx-auto flex justify-between h-20 px-16 py-8 items-center">
+          <p className="flex justify-start gap-2 text-sm font-bold">
+            2024 | Ozan İşgör{" "}
+            <Link href={"https://github.com/ozanisgor"}>
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="lg"
+                className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
+              />
+            </Link>
+          </p>
+          <Link href={"/subscribe"} className="text-sm font-light">
+            Üye Ol
           </Link>
-        </p>
-        <Link href={"/subscribe"} className="text-sm font-light">
-          Üye Ol
-        </Link>
+        </div>
       </div>
     </div>
   );
--- components/layouts/navbar/Navbar.tsx
+++ components/layouts/navbar/Navbar.tsx
@@ -30,76 +30,74 @@ export const Navbar = () => {
     <div
       className={`fixed w-full z-20 text-primary-foreground text-sm font-medium transition-colors duration-300 ease-in-out`}
     >
-      <div
-        className={`${
-          isScrolled ? "bg-primary" : "bg-transparent/40"
-        } flex justify-between items-center mx-auto px-16 py-7 max-w-screen-2xl h-20`}
-      >
-        <div className="">Logo</div>
-        <div className="flex items-center">
-          <div>
-            <NavPages />
-          </div>
-          <div>
-            <svg
-              width="1"
-              height="20"
-              viewBox="0 0 1 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line
-                x1="0.25"
-                x2="0.25"
-                y2="20"
-                stroke="#E5E5E5"
-                strokeWidth="0.5"
+      <div className={`${isScrolled ? "bg-primary" : "bg-transparent/40"}  `}>
+        <div className="max-w-screen-2xl mx-auto flex justify-between items-center w-full px-16 py-7 h-20">
+          <div className="">Logo</div>
+          <div className="flex items-center">
+            <div>
+              <NavPages />
+            </div>
+            <div>
+              <svg
+                width="1"
+                height="20"
+                viewBox="0 0 1 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="0.25"
+                  x2="0.25"
+                  y2="20"
+                  stroke="#E5E5E5"
+                  strokeWidth="0.5"
+                />
+              </svg>
+            </div>
+            <div className="px-4 py-2 flex items-center justify-center h-10 gap-4">
+              <FontAwesomeIcon
+                icon={faYoutube}
+                size="lg"
+                className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
               />
-            </svg>
-          </div>
-          <div className="px-4 py-2 flex items-center justify-center h-10 gap-4">
-            <FontAwesomeIcon
-              icon={faYoutube}
-              size="lg"
-              className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
-            />
-            <FontAwesomeIcon
-              icon={faSquareXTwitter}
-              size="lg"
-              className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
-            />
-            <FontAwesomeIcon
-              icon={faFacebook}
-              size="lg"
-              className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
-            />
+              <FontAwesomeIcon
+                icon={faSquareXTwitter}
+                size="lg"
+                className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
+              />
+              <FontAwesomeIcon
+                icon={faFacebook}
+                size="lg"
+                className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
+              />
 
-            {/* to add beat animation */}
-            {/* <FontAwesomeIcon icon={faYoutube} beatFade /> */}
-          </div>
-          <div>
-            <svg
-              width="1"
-              height="20"
-              viewBox="0 0 1 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line
-                x1="0.25"
-                x2="0.25"
-                y2="20"
-                stroke="#E5E5E5"
-                strokeWidth="0.5"
+              {/* to add beat animation */}
+              {/* <FontAwesomeIcon icon={faYoutube} beatFade /> */}
+            </div>
+            <div>
+              <svg
+                width="1"
+                height="20"
+                viewBox="0 0 1 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="0.25"
+                  x2="0.25"
+                  y2="20"
+                  stroke="#E5E5E5"
+                  strokeWidth="0.5"
+                />
+              </svg>
+            </div>
+            <div className="pl-4 py-2 flex items-center justify-center h-10">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                size="lg"
+                className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
               />
-            </svg>
-          </div>
-          <div className="pl-4 py-2 flex items-center justify-center h-10">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              size="lg"
-              className="text-primary-foreground hover:text-secondary-foreground cursor-pointer"
-            />
+            </div>
           </div>
         </div>
       </div>
