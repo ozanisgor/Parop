12515ef948a69a489ef87f454aecc790d1e81398
ozan isgor <ozanisgor11@gmail.com>
2024-12-18 20:30:10 +0300

    fix: main carousel text index & loop text for each slide

--- components/home/MainCarousel.tsx
+++ components/home/MainCarousel.tsx
@@ -42,35 +42,7 @@ export function MainCarousel() {
   };
 
   return (
-    <div className="mx-auto relative">
-      <div className="flex flex-col lg:bottom-20 bottom-10 items-start absolute w-full z-10 md:left-16 left-4  text-primary-foreground gap-4 md:max-w-lg max-w-72">
-        <Badge
-          className={`text-primary-foreground bg-transparent/30 rounded-lg md:py-1 md:px-3 py-0 px-2 uppercase hover:bg-transparent/30 md:text-xs text-[9px] leading-4 list-none tracking-wider ${roboto.className}`}
-        >
-          <span className="">Ethereum</span>
-        </Badge>
-        <h2 className="xl:text-4xl lg:text-3xl md:text-2xl text-sm font-bold lg:mt-2 xl:mt-3 mt-1 leading-snug drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-          Dev Yatırım Fonu Ethereum ETF&apos;si İçin Önemli Bir Adım Attı
-        </h2>
-        <Separator className="w-8" />
-        <span className="md:text-xs font-normal text-[9px] leading-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-          12.12.2024
-        </span>
-
-        <div className="flex items-center w-full gap-2 lg:mt-16 mt-4">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <span
-              key={index}
-              className={`cursor-pointer ${
-                current === index
-                  ? "w-3 h-3 rounded-full bg-primary-foreground"
-                  : "w-3 h-3 rounded-full bg-secondary-foreground"
-              }`}
-              onClick={() => handleSliderChange(index)}
-            ></span>
-          ))}
-        </div>
-      </div>
+    <div className="mx-auto">
       <Carousel setApi={setApi} className="">
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
@@ -83,6 +55,35 @@ export function MainCarousel() {
                   height={600}
                   className="object-cover w-full h-full brightness-75"
                 />
+                <div className="flex flex-col lg:bottom-20 bottom-10 items-start absolute w-full z-10 md:left-16 left-4  text-primary-foreground gap-4 md:max-w-lg max-w-72">
+                  <Badge
+                    className={`text-primary-foreground bg-transparent/30 rounded-lg md:py-1 md:px-3 py-0 px-2 uppercase hover:bg-transparent/30 md:text-xs text-[9px] leading-4 list-none tracking-wider ${roboto.className}`}
+                  >
+                    <span className="">Ethereum</span>
+                  </Badge>
+                  <h2 className="xl:text-4xl lg:text-3xl md:text-2xl text-sm font-bold lg:mt-2 xl:mt-3 mt-1 leading-snug drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    Dev Yatırım Fonu Ethereum ETF&apos;si İçin Önemli Bir Adım
+                    Attı
+                  </h2>
+                  <Separator className="w-8" />
+                  <span className="md:text-xs font-normal text-[9px] leading-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    12.12.2024
+                  </span>
+
+                  <div className="flex items-center w-full gap-2 lg:mt-16 mt-4">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                      <span
+                        key={index}
+                        className={`cursor-pointer ${
+                          current - 1 === index
+                            ? "w-3 h-3 rounded-full bg-primary-foreground"
+                            : "w-3 h-3 rounded-full bg-secondary-foreground"
+                        }`}
+                        onClick={() => handleSliderChange(index)}
+                      ></span>
+                    ))}
+                  </div>
+                </div>
               </div>
             </CarouselItem>
           ))}
