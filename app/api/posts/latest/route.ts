c4f9368e211a73cbea1f910e345ef83568b9e7d8
ozan isgor <ozanisgor11@gmail.com>
2024-12-19 14:55:43 +0300

    add: get latest posts endpoint

---
+++ app/api/posts/latest/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import Post from "@/models/Post";
+import connect from "@/app/api/mongodb";
+
+export async function GET(req: NextRequest) {
+  await connect();
+
+  try {
+    const posts = await Post.find({})
+      .sort({ createdAt: -1 })
+      .limit(8)
+      .select("titleTR slug content createdAt slug")
+      .lean();
+
+    // Cache for 1 minute
+    // return NextResponse.json(posts, {
+    //   headers: {
+    //     "Cache-Control": "public, max-age=60",
+    //   },
+    // });
+
+    return NextResponse.json(posts);
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Failed to fetch latest posts", error: error.message },
+      { status: 500 }
+    );
+  }
+}
