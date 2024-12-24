1f8c0011b9811c0123e3c445d3b09c779eb42ece
ozan isgor <ozanisgor11@gmail.com>
2024-12-24 14:03:03 +0300

    add: link to grid post images and update spacing

--- components/home/latestPosts/LatestPosts.tsx
+++ components/home/latestPosts/LatestPosts.tsx
@@ -16,7 +16,7 @@ export default async function LatestPosts() {
   const articles = await data.json();
 
   return (
-    <Card className="max-w-screen-2xl w-full xl:px-16 lg:px-12 md:px-8 px-4 mt-20 bg-primary-foreground shadow-none border-0">
+    <Card className="max-w-screen-2xl w-full xl:px-16 lg:px-12 md:px-8 px-4 lg:mt-20 md:mt-16 mt-12 bg-primary-foreground shadow-none border-0">
       <CardHeader className="pl-0 max-md:pr-0 max-md:text-center pb-2">
         <CardTitle className="md:text-4xl text-3xl font-bold text-primary">
           En Son Yazılar
--- components/home/latestPosts/PostsGrid.tsx
+++ components/home/latestPosts/PostsGrid.tsx
@@ -11,17 +11,19 @@ type ArticleProps = {
 
 export const PostsGrid = ({ articles }: { articles: ArticleProps[] }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-5 md:gap-y-12 gap-y-20">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-5 md:gap-y-14 gap-y-20">
       {articles.map((article) => {
         const { titleTR, _id, createdAt, content } = article;
         return (
-          <div key={_id} className="flex flex-col gap-4">
-            <Image
-              src={img}
-              alt="blog"
-              className="rounded-md max-h-72 object-cover w-full"
-            />
-            <p className="text-xs font-normal text-secondary">
+          <div key={_id} className="flex flex-col gap-2">
+            <Link href={`/${_id}`}>
+              <Image
+                src={img}
+                alt="blog"
+                className="rounded-md max-h-72 object-cover w-full"
+              />
+            </Link>
+            <p className="text-xs font-normal text-secondary mt-3">
               {new Intl.DateTimeFormat("tr-TR", {
                 day: "2-digit",
                 month: "2-digit",
@@ -31,10 +33,10 @@ export const PostsGrid = ({ articles }: { articles: ArticleProps[] }) => {
             <Link href={`/${_id}`} className="font-bold text-lg text-secondary">
               <h2>{titleTR}</h2>
             </Link>
-            <p className="text-xs font-normal mt-auto text-secondary">
+            {/* <p className="text-xs font-normal mt-auto text-secondary">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
               tristique, purus nec ultricies efficitur
-            </p>
+            </p> */}
           </div>
         );
       })}
